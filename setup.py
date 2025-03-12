f48e43f29bb45966290f2b4318bcb6e089ff3f1d
Sachin Kumar <sachinkumar18449@iiitmanipur.ac.in>
2025-03-12 11:49:49 +0530

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qamodel",
-    version="0.1.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "torch",
